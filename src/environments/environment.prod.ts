856f2515a8c340413e5a0d602bd3248c98644f8c
ghost <dangth@vng.com.vn>
2022-04-20 14:50:57 +0700

    fix: update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
-  apiUrl: `http://localhost:3000`,
+  apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
+  // apiUrl: `http://localhost:3000`,
 };
