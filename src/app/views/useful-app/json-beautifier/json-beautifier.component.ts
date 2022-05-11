fa6a9159aab77bc757da0b65a7ec3e5ce3cd6fb4
ghost <dangth@vng.com.vn>
2022-05-11 14:56:50 +0700

    update json

--- src/app/views/useful-app/json-beautifier/json-beautifier.component.ts
+++ src/app/views/useful-app/json-beautifier/json-beautifier.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ExampleJSON } from '@app/_helpers/fake.data';
 import { SAVED_JSON, SAVED_JSON_2 } from '@app/_shares/constant';
 import { JsonEditorOptions } from '@maaxgr/ang-jsoneditor';
+import diff from 'diff-sequences';
 
 @Component({
   selector: 'json-beautifier',
@@ -11,20 +12,15 @@ import { JsonEditorOptions } from '@maaxgr/ang-jsoneditor';
 export class JsonBeautifierComponent implements OnInit, OnDestroy {
 
   public editorOptions: JsonEditorOptions;
+  public editorOptions2: JsonEditorOptions;
   public initialData: any;
   public initialData2: any;
   public visibleData: any;
   public visibleData2: any;
 
+  isLoadingResults = false;
+
   constructor() {
-    this.editorOptions = new JsonEditorOptions()
-    this.editorOptions.modes = ['code', 'text', 'tree', 'view'];
-    this.editorOptions.mode = this.editorOptions.modes[0];
-    const sampleJSON = ExampleJSON;
-    this.initialData = this.StringToReadableObject(localStorage.getItem(SAVED_JSON) || sampleJSON);
-    this.initialData2 = this.StringToReadableObject(localStorage.getItem(SAVED_JSON_2) || sampleJSON);
-    this.visibleData = this.initialData;
-    this.visibleData2 = this.initialData2;
   }
 
   StringToReadableObject(s: string) {
@@ -32,15 +28,51 @@ export class JsonBeautifierComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-
+    this.editorOptions = new JsonEditorOptions()
+    this.editorOptions.modes = ['code', 'text', 'tree', 'view'];
+    this.editorOptions.mode = this.editorOptions.modes[0];
+    this.editorOptions2 = new JsonEditorOptions()
+    this.editorOptions2.modes = ['code', 'text', 'tree', 'view'];
+    this.editorOptions2.mode = this.editorOptions2.modes[0];
+    const sampleJSON = ExampleJSON;
+    this.initialData = this.StringToReadableObject(localStorage.getItem(SAVED_JSON) || sampleJSON);
+    this.initialData2 = this.StringToReadableObject(localStorage.getItem(SAVED_JSON_2) || sampleJSON);
+    this.visibleData = JSON.parse(JSON.stringify(this.initialData));
+    this.visibleData2 = JSON.parse(JSON.stringify(this.initialData2))
   }
 
   showJson(d: Event) {
+    if (d.isTrusted) {
+      return;
+    }
     this.visibleData = d;
+  }
+
+  showJson2(d: Event) {
+    if (d.isTrusted) {
+      return;
+    }
+    this.visibleData2 = d;
   }
 
   ngOnDestroy(): void {
     localStorage.setItem(SAVED_JSON, JSON.stringify(this.visibleData));
     localStorage.setItem(SAVED_JSON_2, JSON.stringify(this.visibleData2));
+  }
+
+  findDiff() {
+    const a = JSON.stringify(this.visibleData);
+    const b = JSON.stringify(this.visibleData2);
+    let count = 0;
+    function isCommon(aIndex, bIndex) {
+      return a[aIndex] === b[bIndex];
+    }
+    function foundSubsequence(nCommon, aCommon, bCommon) {
+      // see examples
+      ++count;
+      console.log(nCommon, aCommon, bCommon);
+      // console.log(nCommon, a[aCommon], b[bCommon]);
+    }
+    diff(a.length, b.length, isCommon, foundSubsequence);
   }
 }
