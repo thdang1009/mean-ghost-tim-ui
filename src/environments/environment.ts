69e78425274d4c1e6d76968615e376cb7d1fd660
ghost <dangth@vng.com.vn>
2022-05-31 14:15:24 +0700

    reconfig to heroku backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: `https://ghost-site-be.vercel.app`,
+  apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
   // apiUrl: `http://localhost:3000`,
 };
 
