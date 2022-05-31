684fdd647d358103cb77f54d69c51dbd22d03971
ghost <dangth@vng.com.vn>
2022-05-31 11:10:19 +0700

    Revert "deploy: setup firebase"
    This reverts commit ac99988a7545ff8e34da4318f1d3d2c517e4921c.

--- src/main.ts
+++ src/main.ts
@@ -21,27 +21,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'hammerjs';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAjhqvvT-9WECtltXGcLAq6L8m4LzCD6QU",
-  authDomain: "ghost-site-fe-ae2f4.firebaseapp.com",
-  projectId: "ghost-site-fe-ae2f4",
-  storageBucket: "ghost-site-fe-ae2f4.appspot.com",
-  messagingSenderId: "67382640406",
-  appId: "1:67382640406:web:b1adb5ef254542060b1a37",
-  measurementId: "G-FWH80SC06E"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 if (environment.production) {
   enableProdMode();
