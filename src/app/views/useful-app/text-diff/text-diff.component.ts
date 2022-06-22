1f73db419023b63fbd303dd12cda3967fd579b8c
ghost <dangth@vng.com.vn>
2022-06-22 16:30:03 +0700

    refactor: update text-diff responsive and more css

--- src/app/views/useful-app/text-diff/text-diff.component.ts
+++ src/app/views/useful-app/text-diff/text-diff.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ExampleText, ExampleText2 } from '@app/_helpers/fake.data';
 import { showNoti } from '@app/_shares/common';
 import { SAVED_TEXT, SAVED_TEXT_2 } from '@app/_shares/constant';
@@ -11,6 +11,11 @@ import diff from 'diff-sequences';
   styleUrls: ['./text-diff.component.css']
 })
 export class TextDiffComponent implements OnInit {
+  @ViewChild('text1') text1: ElementRef;
+  @ViewChild('text2') text2: ElementRef;
+
+  @ViewChild('backdrop1') backdrop1: ElementRef;
+  @ViewChild('backdrop2') backdrop2: ElementRef;
 
   public 1: any;
   public visibleData1: any;
@@ -118,12 +123,12 @@ export class TextDiffComponent implements OnInit {
   }
   onScroll1(e) {
     // change it to angular and element is 1
-    // var scrollTop = $textarea.scrollTop();
-    // $backdrop.scrollTop(scrollTop);
+    var scrollTop = this.text1.nativeElement.scrollTop;
+    this.backdrop1.nativeElement.scrollTop = scrollTop ;
   }
   onScroll2(e) {
     // change it to angular and element is 2
-    // var scrollTop = $textarea.scrollTop();
-    // $backdrop.scrollTop(scrollTop);
+    var scrollTop = this.text2.nativeElement.scrollTop;
+    this.backdrop2.nativeElement.scrollTop = scrollTop;
   }
 }
