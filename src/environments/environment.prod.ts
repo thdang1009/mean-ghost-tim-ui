378c4da399f8998fc5819b4d27ed36aca1dd0413
ghost <dangth@vng.com.vn>
2022-09-19 16:50:28 +0700

    revert

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com`,
-  // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
+  // apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com`,
+  apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
   gaCode: 'G-FWH80SC06E'
   // apiUrl: `http://localhost:3000`,
 };
