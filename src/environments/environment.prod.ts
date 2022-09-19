1738c2bbeb337dff9685e888854a4c074a8769b6
ghost <dangth@vng.com.vn>
2022-09-19 19:52:49 +0700

    update to vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 
 export const environment = {
   production: true,
-  // apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com`,
-  apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
-  gaCode: 'G-FWH80SC06E'
+  apiUrl: `https://ghost-site-be.vercel.app`,
+  // apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com/v1`,
+  // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
   // apiUrl: `http://localhost:3000`,
+  gaCode: 'G-FWH80SC06E'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
+  apiUrl: `https://ghost-site-be.vercel.app`,
+  // apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com/v1`,
   // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
   // apiUrl: `http://localhost:3000`,
-  apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com`,
   gaCode: 'G-FWH80SC06E',
 };
 
