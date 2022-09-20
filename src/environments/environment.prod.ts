a092ab90bd0d22d9762ec0b9b480c0e5579c9b94
ghost <dangth@vng.com.vn>
2022-09-20 15:22:40 +0700

    change domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // apiUrl: `https://ghost-site-be.vercel.app`,
-  apiUrl: `https://ec2-18-140-98-243.ap-southeast-1.compute.amazonaws.com/v1/`,
+  apiUrl: `https://dangtrinh.site/v1/`,
   // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
   // apiUrl: `http://localhost:3000`,
   gaCode: 'G-FWH80SC06E'
