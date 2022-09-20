81edb8cf4e17a8ea901d46a6d71d9f292708278a
ghost <dangth@vng.com.vn>
2022-09-20 15:23:59 +0700

    chore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 
 export const environment = {
   production: true,
-  // apiUrl: `https://ghost-site-be.vercel.app`,
-  apiUrl: `https://dangtrinh.site/v1/`,
-  // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
+  apiUrl: `https://dangtrinh.site/v1`,
   // apiUrl: `http://localhost:3000`,
   gaCode: 'G-FWH80SC06E'
 };
