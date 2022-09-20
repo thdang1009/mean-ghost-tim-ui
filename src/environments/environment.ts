3c5224194dbd39aca5a9a211ad69ac71e161e272
ghost <dangth@vng.com.vn>
2022-09-20 14:59:26 +0700

    save code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: `https://ghost-site-be.vercel.app`,
-  // apiUrl: `http://ec2-18-140-248-234.ap-southeast-1.compute.amazonaws.com/v1`,
-  apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
+  apiUrl: `https://ec2-18-140-98-243.ap-southeast-1.compute.amazonaws.com/v1/`,
+  // apiUrl: `https://mean-ghost-site-be.herokuapp.com`,
   // apiUrl: `http://localhost:3000`,
   gaCode: 'G-FWH80SC06E',
 };
