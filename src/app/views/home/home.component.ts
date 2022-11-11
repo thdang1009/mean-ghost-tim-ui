905b87d57d461aae6fa9c30343735bc9682361f1
ghost <dangth@vng.com.vn>
2022-11-11 19:38:05 +0700

    feat(blog): update style for private blog in home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryService } from '@app/_services/category.service';
 import { TagService } from '@app/_services/tag.service';
+import { BookPermission } from '@app/_shares/enum';
 import { AuthService, HomeService, PostService } from '@services/_index';
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ import { AuthService, HomeService, PostService } from '@services/_index';
 })
 export class HomeComponent implements OnInit {
 
+  BookPermission = BookPermission;
   isLogined = false;
   isAdmin = false;
   isLoadingResults = true;
