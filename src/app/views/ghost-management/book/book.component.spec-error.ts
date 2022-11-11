ba07100bdb8ce80b057b22f648bfad893c23e67c
ghost <dangth@vng.com.vn>
2022-11-11 19:48:23 +0700

    fix spec and update css for home blog

--- src/app/views/ghost-management/book/book.component.spec-error.ts
+++ src/app/views/ghost-management/book/book.component.spec-error.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AbstractMockObservableService } from '@app/_helpers/mockservice.service';
+import { BookService } from '@app/_services/book.service';
 
 import { BookComponent } from './book.component';
 
+class MockBookService extends AbstractMockObservableService {
+  getBook() {
+    return this;
+  }
+  getMyBook() {
+    return this;
+  }
+  addBook() {
+    return this;
+  }
+  updateBook() {
+    return this;
+  }
+  deleteBook() {
+    return this;
+  }
+}
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+
+      providers: [
+        {
+          provide: BookService, useValue: new MockBookService()
+        },
+      ]
     })
     .compileComponents();
 
