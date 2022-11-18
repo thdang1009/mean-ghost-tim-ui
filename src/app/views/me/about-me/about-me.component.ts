267dae46eccc2c056d2ef0fe7f69cca6837ca424
ghost <dangth@vng.com.vn>
2022-11-18 12:01:54 +0700

    rever rem code

--- src/app/views/me/about-me/about-me.component.ts
+++ src/app/views/me/about-me/about-me.component.ts
@@ -59,17 +59,16 @@ export class AboutMeComponent implements OnInit, AfterViewInit {
   sendMessageToMe(form: NgForm) {
     this.isRunning = true;
     this.socketService.socket.emit('guess message', form);
-    // this.userService.sendGuestMessage(form)
-    // .subscribe(res => {
-    //   this.isRunning = false;
-    //   if (res && res.id) {
-    //     showNoti('Send success!', 'success');
-    //   }
-    // }, (err) => {
-    //   this.isRunning = false;
-    //   showNoti('Send Fail! ' + err.error, 'danger');
-    // });
-    this.isRunning = false;
+    this.userService.sendGuestMessage(form)
+    .subscribe(res => {
+      this.isRunning = false;
+      if (res && res.id) {
+        showNoti('Send success!', 'success');
+      }
+    }, (err) => {
+      this.isRunning = false;
+      showNoti('Send Fail! ' + err.error, 'danger');
+    });
     // call api save guest message
   }
   scrollTo(s) {
